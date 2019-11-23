7cfa1fb2c7b8d53e391f9ea942b85238d6bd861f
Joe McCraw <joe@showblender.com>
2019-11-22 16:17:22 -0800

    re implemented other features for testing

--- GoogleFirebaseHosting/functions/index.js
+++ GoogleFirebaseHosting/functions/index.js
@@ -7,25 +7,35 @@ const faceapi = require('face-api.js');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// app.use(express.static(path.join(__dirname, './public')))
-// app.use(express.static(path.join(__dirname, '../images')))
-// app.use(express.static(path.join(__dirname, '../media')))
-app.use(express.static(path.join(__dirname, '/models')))
-// app.use(express.static(path.join(__dirname, '../../dist')))
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, '../images')))
+app.use(express.static(path.join(__dirname, '../media')))
+app.use(express.static(path.join(__dirname, './models')))
+app.use(express.static(path.join(__dirname, '../../dist')))
 
 const viewsDir = path.join(__dirname, 'views')
 console.log(viewsDir)
 
+app.get('/', (req, res) => res.redirect('/webcam_face_expression_recognition'))
+app.get('/face_detection', (req, res) => res.sendFile(path.join(viewsDir, 'faceDetection.html')))
+app.get('/face_landmark_detection', (req, res) => res.sendFile(path.join(viewsDir, 'faceLandmarkDetection.html')))
+app.get('/face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'faceExpressionRecognition.html')))
+app.get('/age_and_gender_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'ageAndGenderRecognition.html')))
+app.get('/face_extraction', (req, res) => res.sendFile(path.join(viewsDir, 'faceExtraction.html')))
+app.get('/face_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'faceRecognition.html')))
+app.get('/video_face_tracking', (req, res) => res.sendFile(path.join(viewsDir, 'videoFaceTracking.html')))
+app.get('/webcam_face_detection', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceDetection.html')))
+app.get('/webcam_face_landmark_detection', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceLandmarkDetection.html')))
+app.get('/webcam_face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')))
+app.get('/webcam_age_and_gender_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'webcamAgeAndGenderRecognition.html')))
+app.get('/bbt_face_landmark_detection', (req, res) => res.sendFile(path.join(viewsDir, 'bbtFaceLandmarkDetection.html')))
+app.get('/bbt_face_similarity', (req, res) => res.sendFile(path.join(viewsDir, 'bbtFaceSimilarity.html')))
+app.get('/bbt_face_matching', (req, res) => res.sendFile(path.join(viewsDir, 'bbtFaceMatching.html')))
+app.get('/bbt_face_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'bbtFaceRecognition.html')))
+app.get('/batch_face_landmarks', (req, res) => res.sendFile(path.join(viewsDir, 'batchFaceLandmarks.html')))
+app.get('/batch_face_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'batchFaceRecognition.html')))
 
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
-});
-app.get('/timestamp-cached', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
-    response.send(`${Date.now()}`);
-});
-
-app.get('/webcam_face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')));
+// app.get('/webcam_face_expression_recognition', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')));
 // app.get('/realtime', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')));
 
 // // Create and Deploy Your First Cloud Functions
